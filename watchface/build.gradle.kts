2a2b4563b0c9897c544abd8cdabad08fa7ab643d
amoledwatchfaces <92080649+amoledwatchfaces@users.noreply.github.com>
2025-01-08 16:40:11 +0100

    initial commit
    * add watchface resources

---
+++ watchface/build.gradle.kts
@@ -0,0 +1,37 @@
+import com.android.build.gradle.internal.tasks.factory.dependsOn
+
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.amoledwatchfaces.pixelMinimal"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.amoledwatchfaces.pixelMinimal"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 10000001
+        versionName = "1.0.1"
+
+        manifestPlaceholders["publisher"] = "AndroidStudioKoala-2024.1.2"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            /** We are not using isDebuggable flag as it is not possible to debug Watch Face Format package.
+             * Instead, we debug com.samsung.wear.watchface.runtime (Galaxy Watches) and/or
+             * com.google.wear.watchface.runtime (Pixel Watches)
+             */
+            isDebuggable = false
+        }
+    }
+}
